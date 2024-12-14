6119a19245f14586db1c98d5084b4dd199fada26
Santiago Ochoa de Zabalegui Velasco <146175906+SantiOch@users.noreply.github.com>
2024-12-14 14:01:00 +0100

    Update spark.py

--- SparkCode/spark.py
+++ SparkCode/spark.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 import AveragePrice
 import DataOrganization
 import InvertedIndex
+import FilterByNumber
 import advanced_analysis
 
 def run_spark_analysis(input_path, output_path, operation):
@@ -16,6 +17,8 @@ def run_spark_analysis(input_path, output_path, operation):
         InvertedIndex.inverted_index_max_close(input_path, output_path)
     elif operation == "advanced_analysis":
         advanced_analysis.advanced_analysis(input_path, output_path)
+    elif operation == "filter_by_number":
+        FilterByNumber.filter_by_number(input_path, output_path)
     else:
         print(f"Operación desconocida: {operation}")
         print("Operaciones disponibles: average_price, data_organization, inverted_index, advanced_analysis")
