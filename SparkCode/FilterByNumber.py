a3c7a36c72ea488151a36050f617afe85e9f2f51
Santiago Ochoa de Zabalegui Velasco <146175906+SantiOch@users.noreply.github.com>
2024-12-14 14:01:32 +0100

    Added while loop

--- SparkCode/FilterByNumber.py
+++ SparkCode/FilterByNumber.py
@@ -2,17 +2,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 def filter_by_number(input_path, output_path):
-
     spark = SparkSession.builder.appName("FilterByMinClose").getOrCreate()
-
     df = spark.read.csv(input_path, header=True, inferSchema=True)
 
-    try:
-        min_close = float(input("Introduce el precio mínimo para filtrar los valores de 'Close': "))
-    except ValueError:
-        print("Por favor, introduce un número válido.")
-        spark.stop()
-        return
+    while True:
+        try:
+            min_close = float(input("Introduce el precio mínimo para filtrar los valores de 'Close': "))
+            break
+        except ValueError:
+            pass
 
     filtered_df = df.filter(col("Close") > min_close)
     filtered_df.write.mode("overwrite").csv(output_path, header=True)
